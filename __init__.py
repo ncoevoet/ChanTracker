31911a8cdd13630848ecf969d1059c333e8cc957
Nicolas Coevoet <ncoevoet@users.noreply.github.com>
2016-01-05 22:35:00 +0100

    Update __init__.py
    1.3 release dues to 45 commits since 1.2

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "1.2"
+__version__ = "1.3"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.authors.unknown
